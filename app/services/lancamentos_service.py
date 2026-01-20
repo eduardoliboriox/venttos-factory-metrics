b7d8e2081e4edc29f0aed2ac7326bc505af7fc95
Eduardo Libório <eduardosolenomorizliborio@gmail.com>
2026-01-19 21:08:17 -0400

    feat(core): add new service  structure

---
+++ app/services/lancamentos_service.py
@@ -0,0 +1,21 @@
+from app.repositories import lancamentos_repository
+
+def calcular_absenteismo(hc_padrao, hc_real):
+    if hc_padrao <= 0:
+        return 0
+    return round((hc_padrao - hc_real) / hc_padrao * 100, 2)
+
+def criar_lancamento(dados):
+    absenteismo = calcular_absenteismo(
+        int(dados["hc_padrao"]),
+        int(dados["hc_real"])
+    )
+
+    dados["absenteismo"] = absenteismo
+
+    lancamentos_repository.inserir(dados)
+
+    return {
+        "sucesso": True,
+        "absenteismo": absenteismo
+    }
